5af290b159de50bd98027b94d43e744e5ba42dcd
Ilya <krasnenkov.ilya@inbox.ru>
2023-03-09 16:10:45 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace inputValidation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() { InitializeComponent(); }
+
+        // button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+
+            Regex regex = new Regex(@"(@)");
+
+            MatchCollection email = regex.Matches(Email.Text);
+
+            foreach (Match match in email)
+                count++;
+
+            if (count > 1 || count < 1)
+                MessageBox.Show("Неверный адрес", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
