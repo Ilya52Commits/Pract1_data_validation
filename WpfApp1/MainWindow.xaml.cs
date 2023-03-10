229007b41a83a95f182fca251c6ca4289d8aafde
Ilya <krasnenkov.ilya@inbox.ru>
2023-03-10 19:25:05 +0400

    Переделал код

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,20 +13,61 @@ namespace inputValidation
     {
         public MainWindow() { InitializeComponent(); }
 
-        // button
+        // function of the button
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int count = 0;
-            Regex regex = new Regex(@"(@)");
-            MatchCollection email = regex.Matches(Email.Text);
+            string textIdentiti = Identifier.Text;
+            string textName = Name.Text;
+            string textSurname = Surname.Text;
+            string textPasport = Passport.Text;
+            string textEmail = Email.Text;
 
-            foreach (Match match in email)
-                count++;
+            if (string.IsNullOrWhiteSpace(textIdentiti) ||
+                string.IsNullOrWhiteSpace(textName) ||
+                string.IsNullOrWhiteSpace(textSurname) ||
+                string.IsNullOrWhiteSpace(textPasport) ||
+                string.IsNullOrWhiteSpace(textEmail))
+            {
+                MessageBox.Show("Введены некоректные данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
-            if (count > 1 || count < 1)
-                MessageBox.Show("Неверный адрес", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            else
-                File.AppendAllText("employee.txt", Email.Text);
+            // mail check 
+            if (!textEmail.Contains("@"))
+            {
+                MessageBox.Show("Неверный адрес1", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string[] delimiter = textEmail.Split('@');
+
+            if (delimiter.Length != 2)
+            {
+                MessageBox.Show("Неверный адрес2", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string login = delimiter[0];
+            string domain = delimiter[1];
+
+            char firstSymbol = login[0];
+
+            if (!Regex.IsMatch(firstSymbol.ToString(), @"[a-z]"))
+            {
+                MessageBox.Show("Введены некоректные данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string[] arrDobain = domain.Split('.');
+
+            if (arrDobain.Length != 2)
+            {
+                MessageBox.Show("Неверный адрес3", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            File.AppendAllText("employee.txt", textEmail);
+            MessageBox.Show("Данные сохранены", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
