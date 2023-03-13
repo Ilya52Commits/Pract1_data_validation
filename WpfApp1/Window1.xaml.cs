9d2aec584939f1a5ef36d9bd67deb65ef08254fc
Ilya <krasnenkov.ilya@inbox.ru>
2023-03-13 22:48:54 +0400

    Добавил таймер

--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Threading;
 using inputValidation;
 
 namespace WpfApp1
 {
     public partial class Window1 : Window
     {
-        public Window1() => InitializeComponent();
+        public Window1()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+            timer.Tick += new EventHandler(_timerTick);
+        }
 
+        DispatcherTimer timer = new DispatcherTimer();
         private static string _lineKey = "employee";
         private static int _counter = 0;
         MainWindow mainWindow = new MainWindow(); 
         Window window = new Window();
         DateTime Time;
+        long seconds = 0;  
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -24,12 +33,27 @@ namespace WpfApp1
                 _checkLoginPassword();
         }
 
+        private void _timerTick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if (seconds == 60)
+            {
+                var message = MessageBox.Show($"Уже прошла минута, а вы не ввели данные.\nЗакрыть программу?", "Привышение ожидания", MessageBoxButton.OKCancel);
+                if (message == MessageBoxResult.OK)
+                    Close();
+                seconds = 0;
+            }
+        }
+
         private bool _checkLoginPassword()
         {
             if (Login.Text == _lineKey && Password.Password == _lineKey)
             {
                 mainWindow.Show();
                 window.Close();
+                Close();
+                timer.Stop();
                 return true;
             }
             else
